a9d13e4e26f9c1e56ada9e70f22b1a45cfc9798b
gottbrecht <simo79j5@stud.kea.dk>
2023-03-30 22:26:39 +0200

    packages annotations etc..

---
+++ src/main/java/com/example/wishlist/model/Wishlist.java
@@ -0,0 +1,61 @@
+package com.example.wishlist.model;
+
+public class Wishlist {
+
+    //COMPUTERPART
+    private String pName;
+    private int pPrice;
+    private String pType;
+
+    //USERR
+    private String uName;
+
+    //WISHLIST
+    private int userID;
+    private String listName;
+
+    //WISH
+    private int partID;
+    private int numberCount;
+    private int listID;
+
+    //SHARED
+    private int sharedToUserID;
+
+    public Wishlist(String pName, int pPrice, String pType, String uName,
+                    int userID, String listName, int partID, int numberCount,
+                    int listID, int sharedToUserID) {
+        this.pName = pName;
+        this.pPrice = pPrice;
+        this.pType = pType;
+        this.uName = uName;
+        this.userID = userID;
+        this.listName = listName;
+        this.partID = partID;
+        this.numberCount = numberCount;
+        this.listID = listID;
+        this.sharedToUserID = sharedToUserID;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public int getpPrice() {
+        return pPrice;
+    }
+
+    public String getpType() {
+        return pType;
+    }
+
+    @Override
+    public String toString() {
+        return "Wishlist{" +
+                "pName='" + pName + '\'' +
+                "pPrice='" + pPrice + '\'' +
+                "pType='" + pType + '\'' +
+                '}';
+    }
+
+}
