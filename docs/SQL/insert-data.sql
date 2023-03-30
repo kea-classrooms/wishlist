ada5bf584d320bcdd9002b2a2b88a705f0c6778a
toreh.simonsen@live.dk <tore0834@stud.kea.dk>
2023-03-30 13:30:23 +0200

    Started creating test data

---
+++ docs/SQL/insert-data.sql
@@ -0,0 +1,20 @@
+INSERT INTO COMPUTERPART(pName, pPrice, pType) VALUE ("Gigabyte B450 AORUS M",89.99,"Motherboard");
+INSERT INTO COMPUTERPART(pName, pPrice, pType) VALUE ("ASUS ROG Strix GeForce RTX 3080",1299.99,"GPU");
+INSERT INTO COMPUTERPART(pName, pPrice, pType) VALUE ("AMD Ryzen 9 5900X",549.99,"CPU");
+INSERT INTO COMPUTERPART(pName, pPrice, pType) VALUE ("EVGA SuperNOVA 750W G5",129.99,"Power Supply");
+INSERT INTO COMPUTERPART(pName, pPrice, pType) VALUE ("SteelSeries Arctis 7 Wireless Gaming Headset",149.99,"Headset");
+INSERT INTO COMPUTERPART(pName, pPrice, pType) VALUE ("Elgato Game Capture 4K60 Pro MK.2",249.99,"Capture Card");
+INSERT INTO COMPUTERPART(pName, pPrice, pType) VALUE ("Samsung 970 EVO Plus 1TB NVMe SSD",199.99,"Storage");
+INSERT INTO COMPUTERPART(pName, pPrice, pType) VALUE ("Noctua NH-D15 chromax.black",99.99,"CPU Cooler");
+
+INSERT INTO USERR(uName) VALUE ("Carl");
+INSERT INTO USERR(uName) VALUE ("Simone");
+INSERT INTO USERR(uName) VALUE ("Sadek");
+INSERT INTO USERR(uName) VALUE ("Tore");
+
+INSERT INTO WISH(partID, numberCount) VALUE(3,2);
+INSERT INTO WISH(partID, numberCount) VALUE(1,2);
+INSERT INTO WISH(partID, numberCount) VALUE(8,3);
+INSERT INTO WISH(partID, numberCount) VALUE(2,5);
+INSERT INTO WISH(partID, numberCount) VALUE(6,5);
+INSERT INTO WISH(partID, numberCount) VALUE(2,4);
