1e20ed012edf3c1c185fd5e80da9c4015ae7a84e
Tohasim <toreh.simonsen@live.dk>
2023-04-04 09:12:30 +0200

    Base HTML added to templates, and controller class has been linked to proper template files

--- src/main/java/com/example/wishlist/controller/WishlistController.java
+++ src/main/java/com/example/wishlist/controller/WishlistController.java
@@ -1,10 +1,19 @@
 package com.example.wishlist.controller;
 
+import com.example.wishlist.model.Wish;
+import com.example.wishlist.model.Wishlist;
 import com.example.wishlist.repository.Wishlist_DB_Repository;
 import org.springframework.context.ApplicationContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("wishlist")
@@ -15,4 +24,16 @@ public class WishlistController {
         this.database = (Wishlist_DB_Repository) context.getBean(impl);
     }
 
+    @GetMapping("/add")
+    public String addWishList(Model model){
+        //TODO: wishlist skal addes til model, sammen med de typer af komponenter/andet data vi skal bruge
+        return "WishListUI";
+    }
+
+    @PostMapping("/add")
+    public String wishListSuccess(@ModelAttribute("wishlist") Wishlist wishlist){
+        System.out.println(wishlist);
+        //TODO: Skal kobles til database metode der poster ønskelisten
+        return "Wish_Success";
+    }
 }
