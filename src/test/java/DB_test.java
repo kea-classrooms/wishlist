f1b9094b04fcf6fd55200948f5c837b679858cc8
gottbrecht <simo79j5@stud.kea.dk>
2023-04-17 11:49:14 +0200

    UnitTest

---
+++ src/test/java/DB_test.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DB_test {
+    DB_test db;
+    // Udføres før hver test
+    @BeforeEach
+    void setUp() {
+        db= new DB_test();
+    }
+
+    @Test
+    void getConnection() {
+        assertTrue(false);	// test fejler
+    }
+
+    @Test
+    void find() {
+        assertTrue(false);	// test fejler
+
+    }
+
+
+    @Test
+    void sletPerson() {
+        assertTrue(false);	// test fejler
+
+    }
+}
